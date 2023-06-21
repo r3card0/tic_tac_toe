0cb8f267d3ab40c3abd03e76454319b7400bdd94
r3card0 <rsaldivar@1MXLRODRIGUE2.ad.here.com>
2023-06-21 10:23:14 -0500

    logic with oop

---
+++ pre_class.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+class Tictactoe:
+    def __init__(self) -> None:
+        self.board = [
+            ["","",""],
+            ["","",""],
+            ["","",""],
+        ]
+
+    def show_board(self):
+        print("") # lefting a space
+        
+        for line in self.board: # print line by line
+            print(line)
+
+        print("") # lefting a space
+
+    def game(self): 
+        self.show_board() # call show_board function
+
+        while True:
+            option = str(input(''))
+            if option == 'salir':
+                break
